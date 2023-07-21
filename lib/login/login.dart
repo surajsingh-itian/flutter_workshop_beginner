9e8d31ff5b8aeaf51fd583c207ac80c5b5aaf3c6
Suraj Singh <surajsingh.itian@gmail.com>
2023-07-22 00:47:42 +0530

    feat(page): add login form with basic validation

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -8,8 +8,70 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final data = {'username': '', 'password': ''};
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login')),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(
+                  autofocus: true,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Username',
+                  ),
+                  onSaved: (newValue) => data['username'] = newValue ?? '',
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Username is required';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Password',
+                  ),
+                  obscureText: true,
+                  onSaved: (newValue) => data['password'] = newValue ?? '',
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Password is required';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 24.0),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _formKey.currentState!.save();
+
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Processing Data: $data'),
+                        ),
+                      );
+                    }
+                  },
+                  child: const Text('Login'),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
